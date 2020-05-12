203a44af9cbc474d75deebfb2bab4706195326fc
dhiep <dhiepp@users.noreply.github.com>
2020-05-12 20:55:42 +0700

    database url for heroku config var

--- server/data/Connection.js
+++ server/data/Connection.js
@@ -1,7 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 
 // Connection URL and database name
-const url = 'mongodb://localhost:27017';
+// const url = 'mongodb://localhost:27017';
+// Heroku config var
+const url = process.env.DATABASE_URL;
 const dbName = 'neshap';
 let client;
 
